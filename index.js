901dd1c93f471ef6c65140c793d405745d0c2af3
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2021-02-27 13:13:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -270,6 +270,27 @@ function ativarFilters(botao) {
     document.getElementById("filters-" + botao).classList.add("filter-checked")
 }
 
+// Filters 
+function filter() {
+
+    var filterValue, input, ul, li, a, i;
+    input = document.getElementById("search");
+    filterValue = input.value.toUpperCase();
+    ul = document.getElementById("Menu");
+    li = ul.getElementsByTagName("li");
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+            li[i].style.display = "";
+
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
 
 
 
@@ -338,3 +359,4 @@ function changeOpacity() {
 }
 
 window.onload = changeOpacity()
+
